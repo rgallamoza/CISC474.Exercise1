55676ae82c4063581a1d14677b4b43fe7ab60023
rgallamoza <ryan.gallamoza@gmail.com>
2019-09-07 00:13:36 -0400

    Jumbotron displays upon form submission

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 $(function(){
     //this code runs after page is fully loaded
+    $('#nameform').on('submit', function(evt){
+        evt.preventDefault();
+        $('.jumbotron').removeClass('d-none');
+        $('.jumbotron').show();
+        return false;
     });
+});
